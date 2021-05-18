569f129f704aa9ca9487abbfef05bfd5dacbc44f
RaghavendraSR06 <raghu99459@gmail.com>
2021-05-18 19:08:51 +0530

    Update Measurements.cs

--- Measurements/Measurements.cs
+++ Measurements/Measurements.cs
@@ -15,14 +15,29 @@ namespace BatteryMonitoring
 
         public static Dictionary<string,int> GetReadingRange(List<int> readings)
         {
-            Dictionary<string, int> expectedRanges = new Dictionary<string, int>();
-            if (!IsReadingsAvailable(readings))
+           Dictionary<string, int> ranges = new Dictionary<string, int>();
+            values.Sort();
+            int count = 1;
+            int first = values[0];
+            int last = values[0];
+            int current = values[0];
+
+            for (int i = 1; i < values.Count(); i++)
             {
-                return expectedRanges;
+                if (current == values[i] || current + 1 == values[i])
+                {
+                    current = last = values[i];
+                    count++;
+                }
+                else
+                {
+                    ranges.Add(first + "-" + last, count);
+                    current = last = first = values[i];
+                    count = 1;
+                }
             }
-            expectedRanges.Add("3-5", 4);
-            expectedRanges.Add("10-12", 3);
-            return expectedRanges;
+            ranges.Add(first + "-" + last, count);
+            return ranges;
         }
     }
 }
